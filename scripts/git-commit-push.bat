5a1d61b5f532228e3ae60a27e810c81a4e3e0083
stonkskoding-cmd <stonkskoding@gmail.com>
2026-06-16 22:44:56 +0300

    chore: Update git commit helper script

--- scripts/git-commit-push.bat
+++ scripts/git-commit-push.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /d "c:\Users\User-pc\Desktop\online-school"
 git add .
-git commit -F .git-commit-msg.txt
+git commit -F .commit-msg-tmp.txt
 if errorlevel 1 exit /b 1
+del /q .commit-msg-tmp.txt 2>nul
 git push
-del /q .git-commit-msg.txt 2>nul
 exit /b %errorlevel%
